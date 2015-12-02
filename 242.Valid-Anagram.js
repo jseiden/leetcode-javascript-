c64eaa3a28a676abbab0da59beda21292e8ad699
jseiden <jseiden@gmail.com>
2015-12-01 16:18:23 -0800

    Valid-Anagram

---
+++ 242.Valid-Anagram.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) { 
+  return s.split("").sort().join("") === t.split("").sort().join("");
+};
+
