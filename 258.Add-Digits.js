f9deee9b8f43d4a7bff26791fa3941fccb324c0a
jseiden <jseiden@gmail.com>
2015-12-15 01:18:25 -0800

    add prompt

--- 258.Add-Digits.js
+++ 258.Add-Digits.js
@@ -1,3 +1,9 @@
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// For example:
+
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
 /**
  * @param {number} num
  * @return {number}
