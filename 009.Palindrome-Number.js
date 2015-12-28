b0362da7c05ace63bab59092c47d5526c6970c6e
jseiden <jseiden@gmail.com>
2015-12-27 19:44:10 -0800

    add arrow function to Palindrome-Number

--- 009.Palindrome-Number.js
+++ 009.Palindrome-Number.js
@@ -1,7 +1,11 @@
+// Determine whether an integer is a palindrome. Do this without extra space.
+
 /**
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    return "" + x === x.toString().split("").reverse().join("");
-};
+
+//var isPalindrome = function(x) {
+    //return "" + x === x.toString().split("").reverse().join("");
+//};
+var isPalindrome = x => "" + x === x.toString().split("").reverse().join("");
