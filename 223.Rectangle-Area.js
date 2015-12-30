884d68a22584134321f3992b9a2057e0a6521a35
jseiden <jseiden@gmail.com>
2015-12-29 22:50:26 -0800

    Rectangle prompt

---
+++ 223.Rectangle-Area.js
@@ -0,0 +1,24 @@
+/*
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+
+Rectangle Area
+Assume that the total area is never beyond the maximum possible value of int.
+*/
+
+/**
+ *
+ * @param {number} A
+ * @param {number} B
+ * @param {number} C
+ * @param {number} D
+ * @param {number} E
+ * @param {number} F
+ * @param {number} G
+ * @param {number} H
+ * @return {number}
+ */
+var computeArea = function(A, B, C, D, E, F, G, H) {
+    
+};
