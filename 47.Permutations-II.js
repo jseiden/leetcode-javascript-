5d747ff0abcd173743d4f7d2dfaa08fe096b16d5
jseiden <jseiden@gmail.com>
2015-12-31 23:32:57 -0800

    perm prompt

---
+++ 47.Permutations-II.js
@@ -0,0 +1,16 @@
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[1,1,2], [1,2,1], and [2,1,1].
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+  
+};
+
