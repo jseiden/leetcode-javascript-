daf8b9b1bc5274471de5962484bbdd65f7ea29bd
jseiden <jseiden@gmail.com>
2016-01-07 22:33:47 -0800

    add prompt

---
+++ 119.Pascals-Triangle-II.js
@@ -0,0 +1,12 @@
+// Given an index k, return the kth row of the Pascal's triangle.
+
+// For example, given k = 3,
+// Return [1,3,3,1].
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    
+};
