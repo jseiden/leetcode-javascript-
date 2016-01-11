deaa7d607dfdc20c1eeac5f9b5931efb1083ada3
jseiden <jseiden@gmail.com>
2016-01-10 23:55:39 -0800

    add Palindrome-Linked-List

---
+++ 234.Palindrome-Linked-List.js
@@ -0,0 +1,16 @@
+// Given a singly linked list, determine if it is a palindrome.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+};
