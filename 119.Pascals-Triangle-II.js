42d11ab85da9aa1c8f24b23e875324b46df7954e
jseiden <jseiden@gmail.com>
2016-01-11 23:51:12 -0800

    Pascals-Triangle-II

--- 119.Pascals-Triangle-II.js
+++ 119.Pascals-Triangle-II.js
@@ -8,5 +8,30 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    
+  var res = [];
+  var k;
+
+  for (k = 0; k <= rowIndex; k++) {
+    res = makeRow(res);
+  }
+
+  return res;
 };
+
+// fn to make next row 
+function makeRow(prev) {
+  var row = [];
+
+  if (prev.length === 0) {
+    return [1];
+  } else {
+    for (var i = 0; i < prev.length - 1; i++) {
+      row.push(prev[i] + prev[i+1]);
+    }
+  }
+
+  row.push(1);
+  row.unshift(1);
+
+  return row;
+}
