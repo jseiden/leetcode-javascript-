7c26ad2b1bb823c1ce6b4e3c7298a27035d9258c
jseiden <jseiden@gmail.com>
2016-01-18 04:44:10 -0800

    Power-of-Three

--- 326.Power-of-Three.js
+++ 326.Power-of-Three.js
@@ -5,5 +5,12 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    
+  if (n/3 === 1 || n === 1) {
+    return true;
+  } else if (n < 3) {
+    return false;
+  } else {
+    return isPowerOfThree(n/3);
+  }
 };
+
