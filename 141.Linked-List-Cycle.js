1457c72b1ce7ee52dd6db933619d0b815664e715
jseiden <jseiden@gmail.com>
2016-01-31 23:38:47 -0800

    add prompt

---
+++ 141.Linked-List-Cycle.js
@@ -0,0 +1,18 @@
+// Given a linked list, determine if it has a cycle in it.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+
+    
+};
