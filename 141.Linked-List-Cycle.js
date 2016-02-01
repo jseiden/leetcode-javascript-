288f747e5fd4eda1c8f714bd520fe02cab8dfcfb
jseiden <jseiden@gmail.com>
2016-02-01 00:16:55 -0800

    Linked-List-Cycle

--- 141.Linked-List-Cycle.js
+++ 141.Linked-List-Cycle.js
@@ -13,6 +13,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+  var node1 = head;
+  var node2 = head;
 
-    
+  while (node2 && node2.next) {
+    node1 = node1.next;
+    node2 = node2.next.next;
+
+    if (node1 === node2) {
+      return true;
+    }
+  }
+
+  return false;
 };
